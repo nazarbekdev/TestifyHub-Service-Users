6169fe00578d7d3f6ea5e169be290c6e6ee367c1
nazarbekdev <nqobulov571@gmail.com>
2024-11-25 18:40:28 +0500

    ♻️ changed: media url

--- root/settings.py
+++ root/settings.py
@@ -122,12 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DOCUMENT_ROOT = os.path.join(BASE_DIR, 'document')
+
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
